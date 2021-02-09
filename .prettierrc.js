2ceb208864393a96475c483fcbe6d6e60ebe4f29
Y-qwq <yezijunqwq@gmail.com>
2021-02-09 17:32:17 +0800

    style

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "endOfLine": "lf",
-  "arrowParens": "avoid",
-  "proseWrap": "always",
-  "trailingComma": "es5",
-  "useTabs": false,
-  "tabWidth": 2,
-  "jsxSingleQuote": false,
-  "semi": true,
-  "singleQuote": true,
-  "printWidth": 100
-}
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  proseWrap: 'always',
+  trailingComma: 'es5',
+  useTabs: false,
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+};
